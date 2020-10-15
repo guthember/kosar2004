70c8cc720ef11e15f12d3562f609579ba0e11aaa
guthember <guthember@gmail.com>
2020-10-15 09:56:17 +0200

    Második feladt elkészítése

--- kosar2004/Program.cs
+++ kosar2004/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,32 @@ namespace kosar2004
 {
   class Program
   {
+    static List<Meccs> meccsek = new List<Meccs>();
+
+    static void MasodikFeladat()
+    {
+      StreamReader file = new StreamReader("eredmenyek.csv");
+
+      file.ReadLine();
+
+      while (!file.EndOfStream)
+      {
+        string[] adat = file.ReadLine().Split(';');
+
+        meccsek.Add(
+                    new Meccs(adat[0], adat[1], Convert.ToInt32(adat[2]), Convert.ToInt32(adat[3]),
+                              adat[4], adat[5])
+                    );
+      }
+
+      file.Close();
+    }
+
+
     static void Main(string[] args)
     {
-      var m = new Meccs("7up Joventut", "Adecco Estudiantes", 81, 73,
-        "Palacio Mun. De Deportes De Badalona", "2005-04-03");
+      MasodikFeladat();
 
-      Console.WriteLine($"{m.Hazai} - {m.Idegen} ({m.HPont}:{m.IPont})" );
 
       Console.ReadLine();
     }
